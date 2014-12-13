bfd358d68aea082e09babb5e51f924cfc0586b73
Jun Clemente <kjstudios858@gmail.com>
2014-12-13 10:53:41 -0800

    removed debugging lines from views.py; added comments to my_functions.py

--- app/my_functions.py
+++ app/my_functions.py
@@ -19,12 +19,17 @@ def convert_1_to_3(aa_chain, d_forms):
     if not d_forms:
         aa_chain = aa_chain.upper()
 
-    aa_chain3 = ''
-    aa_stats = {}
+    aa_chain3 = ''  # Converted aa_chain to 3-letter representation
+    aa_stats = {}  # Dict of AA used and how many of each in aa_chain
 
     for aa in aa_chain:
+        # searches AA in aa_dict and appends to aa_chain3 if it exists
+        # if not found, process stops and an error is returned
         if aa in aa_dict:
             aa_chain3 += aa_dict[aa][0] + '-'
+
+            # adds AA to aa_stats and calculates the total number of times
+            # each AA is used
             if aa_dict[aa][1] not in aa_stats:
                 aa_stats[aa_dict[aa][1]] = 1
             else:
@@ -34,9 +39,13 @@ def convert_1_to_3(aa_chain, d_forms):
                             Please check your entry."
             break
 
+    # Removes hyphen after last AA
     aa_chain3 = aa_chain3[:-1]
 
-    result = {'chain': aa_chain, 'length': aa_length, 'conversion': aa_chain3,
-              'aa_stats': aa_stats}
+    result = {'chain': aa_chain,  # original AA chain being converted
+              'length': aa_length,  # length of AA chain
+              'conversion': aa_chain3,  # AA chain in 3-letter representation
+              'aa_stats': aa_stats  # Stats of AA chain
+              }
 
     return result
--- app/views.py
+++ app/views.py
@@ -7,20 +7,11 @@ from my_functions import convert_1_to_3
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # global amino_acid_dict
     result = {}
-
     form = ConvertOneForm()
 
-    # print len(aa_dict)
-
     if form.validate_on_submit():
-        # print form.aa_string.data
-        # print form.d_forms.data
         result = convert_1_to_3(form.aa_string.data, form.d_forms.data)
-        # print result
-        # return render_template('index.html', aa_dict=aa_dict, title='Peptide \
-        #                        Sequence Conversion Tool', form=form)
 
     return render_template('index.html', aa_dict=aa_dict,
                            result=result, form=form)
